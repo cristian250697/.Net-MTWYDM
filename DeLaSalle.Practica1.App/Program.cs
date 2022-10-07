90ede8499c78f82e5529fc92144b2b5f7ca093d9
Angel Christian Alvarez Trujillo <cristian_alvarez2506@outlook.com>
2022-10-06 23:23:32 -0500

    First practice V1.1 MTWYDM 2022

---
+++ DeLaSalle.Practica1.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DeLaSalle.Practica1.Core.Entities;
+using DeLaSalle.Practica1.Core.Services;
+using DeLaSalle.Practica1.Core.Managers;
+
+namespace DeLaSalle.Practica1.App;
+
+public static class Program
+{
+    public static void Main (string [] args) 
+    {
+        float weight = 0;
+
+        System.Console.WriteLine("Enter your Weight in Earth [KG]:");
+        Single.TryParse(System.Console.ReadLine(), out weight);
+
+        var person = new Person{Weight = weight};
+
+        var service = new ConversorService();
+        var manager = new ConversorManager(service);
+
+        Conversor cnv = manager.GetConversor(person);
+
+        person.WeightOnMars = cnv.WeightOnMars;
+
+        System.Console.WriteLine($"Your weight in Mars is {person.WeightOnMars}");
+
+    }
+}
